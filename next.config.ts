ddd8df8515ce016d6dfdc0162624a9d10e64e4ea
kkkjin1 <jnl71@kakao.com>
2026-07-07 20:20:46 +0900

    perf: webpack 강제 제거 + transpilePackages 정리 - Turbopack 복원으로 빌드 속도 개선

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  transpilePackages: [
-    '@tiptap/core',
-    '@tiptap/react',
-    '@tiptap/starter-kit',
-    '@tiptap/pm',
-    '@tiptap/extensions',
-    '@tiptap/extension-blockquote',
-    '@tiptap/extension-bold',
-    '@tiptap/extension-bubble-menu',
-    '@tiptap/extension-bullet-list',
-    '@tiptap/extension-code',
-    '@tiptap/extension-code-block',
-    '@tiptap/extension-color',
-    '@tiptap/extension-document',
-    '@tiptap/extension-dropcursor',
-    '@tiptap/extension-floating-menu',
-    '@tiptap/extension-gapcursor',
-    '@tiptap/extension-hard-break',
-    '@tiptap/extension-heading',
-    '@tiptap/extension-highlight',
-    '@tiptap/extension-horizontal-rule',
-    '@tiptap/extension-italic',
-    '@tiptap/extension-link',
-    '@tiptap/extension-list',
-    '@tiptap/extension-list-item',
-    '@tiptap/extension-list-keymap',
-    '@tiptap/extension-ordered-list',
-    '@tiptap/extension-paragraph',
-    '@tiptap/extension-strike',
-    '@tiptap/extension-text',
-    '@tiptap/extension-text-style',
-    '@tiptap/extension-underline',
-  ],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
